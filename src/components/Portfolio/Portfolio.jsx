b4dfa1b516cdee68820dd3670599ebee47396837
kashanafzl <kashanafzal2221@gmail.com>
2024-08-09 14:55:53 +0500

    new

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,11 +2,14 @@ import React, { useContext } from "react";
 import "./Portfolio.css";
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css";
-import Sidebar from "../../img/sidebar.png";
+import Sidebar from "../../img/p1.png";
 import Ecommerce from "../../img/ecommerce.png";
 import HOC from "../../img/hoc.png";
 import MusicApp from "../../img/musicapp.png";
 import { themeContext } from "../../Context";
+
+
+
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
@@ -24,7 +27,9 @@ const Portfolio = () => {
         className="portfolio-slider"
       >
         <SwiperSlide>
+          <a href="https://kashanafzl.github.io/Shruh/User-Management.html" target="_blank" rel="noopener noreferrer">
           <img src={Sidebar} alt="" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
           <img src={Ecommerce} alt="" />
