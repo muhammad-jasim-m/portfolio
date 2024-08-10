7c2dfbd97a366eb6e1c2e4d87b98399fe1eaa7be
kashanafzl <kashanafzal2221@gmail.com>
2024-08-10 16:54:24 +0500

    addfooter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./Footer.css";
 import Wave from "../../img/footer2.png";
 import Github from "../../img/gihubwhite.png";
@@ -6,6 +6,25 @@ import LinkedIn from "../../img/linkedwhite.png";
 import Instagram from "../../img/whatsappwhite.png";
 
 const Footer = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 300) { // Adjust this value to control when the button appears
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <div className="footer">
       <img src={Wave} alt="" style={{ width: "105%" , marginLeft : '-3rem' }} />
@@ -19,9 +38,16 @@ const Footer = () => {
           <a href="   https://www.linkedin.com/in/kashan-afzal/" target="_blank" rel="noopener noreferrer">
             <img className="githubimg" src={LinkedIn} alt="" />
           </a>
+          <a href="https://wa.me/923139614220" target="_blank" rel="noopener noreferrer">
           <img className="githubimg" src={Instagram} alt="" />
+          </a>
         </div>
       </div>
+      isVisible && (
+      <button className="scroll-to-top" onClick={scrollToTop}>
+        &#8679; {/* This is an arrow up symbol; you can use an icon or text */}
+      </button>
+    )
     </div>
   );
 };
--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -37,7 +37,7 @@ const Testimonial = () => {
       <div className="t-heading">
         <span>Clients always get </span>
         <span>Exceptional Work </span>
-        <span>from me...</span>
+        <span>from me.</span>
       {/* <div className="blur t-blur1" style={{ background: "var(--purple)" }}></div>
       <div className="blur t-blur2" style={{ background: "skyblue" }}></div> */}
 
